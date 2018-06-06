d8d1d2257f9e3eb64ec4fd86e766f98565e206c6
meehantr <meehantr@rose-hulman.edu>
2018-06-06 10:40:14 -0500

    m2

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -254,7 +254,7 @@ def tag():
 
 ###############################################################################
 #
-# done
+# Done
 #   COMMIT-and-PUSH your work (after changing this TO-DO to DONE).
 #
 #   As a reminder, here is how you should do so:
